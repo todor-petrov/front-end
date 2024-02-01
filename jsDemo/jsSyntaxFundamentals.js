6a4c6a3e422d7fe7f24faeb5f00762b18409988f
Todor Petrov <106479236+todor-petrov@users.noreply.github.com>
2024-02-01 22:27:38 +0200

    Add files via upload

---
+++ jsDemo/jsSyntaxFundamentals.js
@@ -0,0 +1,16 @@
+// 01. Multiply Number By 2 
+/*
+function mulipyNumberByTwo(muliplierOne) {
+    const muliplierTwo = 2;
+    let multiply = muliplierOne * muliplierTwo;
+    console.log(multiply)
+}
+*/
+
+// 02. Student Information
+/*
+function studentInformation(studentName, studentAge, studentGrade) {
+    let studentInfo = `Name: ${studentName}, Age: ${studentAge}, Grade: ${studentGrade.toFixed(2)}`
+    console.log(studentInfo)
+}
+*/
